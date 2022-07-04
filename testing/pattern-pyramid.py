539ccac42dd9aeecbe0a71fd408e718136eb0e95
Silky <silky.jain1702@gmail.com>
2022-07-04 12:35:22 -0400

    pyramid program

---
+++ testing/pattern-pyramid.py
@@ -0,0 +1,8 @@
+tr = int(input("enter number of rows: ")) # total number of rows
+sym = input("Enter the symbol to print: ")
+r = 1  # row number initial
+while r <= tr:
+    s = tr - r  # calculating number of space to leave before printing symbol
+    n = (2 * r) - 1  # how many times to print symbol
+    print(" " * s, sym * n)
+    r += 1
