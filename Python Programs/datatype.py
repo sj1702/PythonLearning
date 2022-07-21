0bb574f66847ebd26af7615e079fdf7d1a80f95d
Silky <silky.jain1702@gmail.com>
2022-07-21 15:53:04 -0400

    21july

--- Python Programs/datatype.py
+++ Python Programs/datatype.py
@@ -1,5 +1,57 @@
 print(2+2)
-
-
-
+# 4
+print(1.5 + 2.25)
+# 3.75
+print(4 - 2)
+# 2
+print(100 - .5)
+# 99.5
+print(9 - 10)
+# -1
+print(2 * 3)
+# 6
+# doing simple maths in python. Mind the spaces before
+# and after operators
+print(4 / 2)
+# 2.0
+print(1 / 2)
+# 0.5
+# Single / gives the division answers in float type.
+print((1 + 2) * 3)
+# 9
+x = 4
+print(x * 2)
+# 8     doing Algebra
 print(type(1))
+#<class 'int'>
+print(type(1.0))
+#<class 'float'>
+print(type("Hello"))
+print(type("23"))
+#<class 'str'>
+#<class 'str'>   class string   always to be in double quotes or single quotes
+print(type('Hello'))
+#<class 'str'>
+print("hello" + "world")
+#helloworld
+print("hello " + "world")
+print("hello" + " " + "world")
+#hello world
+#hello world
+#print("hello " + 1)
+# Traceback (most recent call last):
+ #File "C:\Users\Silky\PycharmProjects\mylearning\Python Programs\datatype.py", line 41, in <module>
+  # print("hello " + 1)
+ #  TypeError: can only concatenate str (not "int") to str.
+print(str(1))
+#1
+print("hello " + str(1))
+#hello 1
+
+""" python uses Interpretor so if there is an error it will not run next line . So when running above program first make line 41 as comment"""
+print(len('Hello'))
+#5
+print(len('Hello world'))
+#11  len() tells length of the string. It includes whitespaces also.
+print('A' * 5)
+#AAAAA
