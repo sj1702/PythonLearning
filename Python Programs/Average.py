6c1445521c3dc167402b9e86d1324895f46e9565
sj1702 <107590591+sj1702@users.noreply.github.com>
2023-03-14 20:09:11 +0530

    Update Average.py

--- Python Programs/Average.py
+++ Python Programs/Average.py
@@ -2,8 +2,8 @@ n = int(input("Enter the value of n? "))
 sum=0  
 count = 1  
 while count <= n:  
-    x = int(input("Enter the %d number? "%(count)))  /* In Python both %s and %d are placeholders for a string and a number respectively. */
-                                          /* %s will return the string and %d will return number, the values are passed using % operator. */
+    x = int(input("Enter the %d number? "%(count)))  # In Python both %s and %d are placeholders for a string and a number respectively.
+                                          # %s will return the string and %d will return number, the values are passed using % operator. 
     sum = sum + x  
     count = count + 1  
 average  = sum / n  
