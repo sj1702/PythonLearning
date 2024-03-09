7fe33494c05bc9111f4164375056e5eb6900bbe8
silkyjain <jain.silky1702@gmail.com>
2024-03-09 00:16:59 -0500

    files edited

--- Day 01 Exercise.py
+++ Day 01 Exercise.py
@@ -1,17 +1,25 @@
 # Exercise 1: Write a Python program that takes a string as input and converts it to upper case.
+n = str(input("Enter the value of n? "))
+print(n.upper())
 
 
 # Exercise 2: Write a Python program that takes a string as input and converts it to lower case.
-
+n = str(input("Enter the value of n? "))
+print(n.lower())
 
 # Exercise 3: Write a Python program that takes a string as input and converts it to title case.
-
+n = str(input("Enter the value of n? "))
+print(n.title())
 
 # Exercise 4: Write a Python program to concatenate two strings.
-
+a = "Milk"
+b = "Man"
+print(a + b)
 
 # Exercise 5: Write a Python program that takes two strings as input from the user and concatenates them.
-
+a = str(input("Enter the value of a? "))
+b = str(input("Enter the value of b? "))
+print(a + b)
 
 # Exercise 6: Write a Python program that converts a string to an integer.
 
--- Day 01 Mini Project.py
+++ Day 01 Mini Project.py
@@ -3,37 +3,53 @@ Create a Python program that takes a user input string and converts
 it to uppercase, lowercase, and title case. '''
 
 # Solution
-
+print("Welcome to Text Converter")
 # Greeting Message
+n = str(input("Enter the text "))
 
 # Taking User Input 
-
+print(n.upper())
 # Converting into Upper Case
-
-# Out Put
+print(n.lower())
+# Converting into Lower Case
+print(n.title())
+# Converting into Title Case
+'''Welcome to Text Converter
+Enter the text Milkman
+MILKMAN
+milkman
+Milkman
+# Out Put'''
 
 '''2. Brand Name Generator:
 Create a Python program that takes a user input their birth place name  and pet name. 
 After that it will combine them & give the new one as the brand name. '''
 
 # Solution
-
+print("Welcome to Brand Name Generator")
 # Greeting Message
-
+bpn = str(input("Enter your BirthPlace Name: "))
+pn = str(input("Enter your Pet Name: "))
 # Taking User Input (Birth Place Name, Pet Name)
-
+bn = bpn+pn
 # Combining them to give the brand name
-
-# Out Put
+print("Brand Name for you: " +bn)
+'''# Out Put
+Welcome to Brand Name Generator
+Enter your BirthPlace Name: linux
+Enter your Pet Name: world
+Brand Name for you: linuxworld'''
 
 '''3. Tip Calculator:
 Create a Python program that takes a user input their bill, % of tip they want to give & the number of people.
 After that it will give the exact amount which all have to pay, along with tip.'''
 
 # Solution
-
+print("Welcome to Brand Name Generator")
 # Greeting Message
-
+bill = float(input("Enter your Bill amount: "))
+tip = float(input("Enter the tip percent: "))
+count = int(input("Enter total number of person: "))
 # Taking User Input (bill, tip,number of people)
 
 # Calculating the total bill including tip
