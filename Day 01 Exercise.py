7d0b817c02443c680992466e9ba596b9b4842463
silkyjain <jain.silky1702@gmail.com>
2024-03-08 23:33:32 -0500

    3 new files added and 3 files renamed

---
+++ Day 01 Exercise.py
@@ -0,0 +1,34 @@
+# Exercise 1: Write a Python program that takes a string as input and converts it to upper case.
+
+
+# Exercise 2: Write a Python program that takes a string as input and converts it to lower case.
+
+
+# Exercise 3: Write a Python program that takes a string as input and converts it to title case.
+
+
+# Exercise 4: Write a Python program to concatenate two strings.
+
+
+# Exercise 5: Write a Python program that takes two strings as input from the user and concatenates them.
+
+
+# Exercise 6: Write a Python program that converts a string to an integer.
+
+
+# Exercise 7: Write a Python program that converts a float to an integer.
+
+
+# Exercise 8: Write a Python program that converts an integer to a string.
+
+
+# Exercise 9: Write a Python program to perform addition, subtraction, multiplication, and division of two numbers.
+
+
+# Exercise 10: Write a Python program to calculate the area of a rectangle given its length and width.
+
+
+# Exercise 11: Write a Python program that takes user input and displays it using f-strings.
+
+
+# Exercise 12: Write a Python program that calculates and displays the result of an arithmetic expression using f-strings.
---
+++ Day 01 Mini Project.py
@@ -0,0 +1,43 @@
+'''1. Capital Text Converter:
+Create a Python program that takes a user input string and converts 
+it to uppercase, lowercase, and title case. '''
+
+# Solution
+
+# Greeting Message
+
+# Taking User Input 
+
+# Converting into Upper Case
+
+# Out Put
+
+'''2. Brand Name Generator:
+Create a Python program that takes a user input their birth place name  and pet name. 
+After that it will combine them & give the new one as the brand name. '''
+
+# Solution
+
+# Greeting Message
+
+# Taking User Input (Birth Place Name, Pet Name)
+
+# Combining them to give the brand name
+
+# Out Put
+
+'''3. Tip Calculator:
+Create a Python program that takes a user input their bill, % of tip they want to give & the number of people.
+After that it will give the exact amount which all have to pay, along with tip.'''
+
+# Solution
+
+# Greeting Message
+
+# Taking User Input (bill, tip,number of people)
+
+# Calculating the total bill including tip
+
+# Calculating amount for every single  person
+
+# Out Put
