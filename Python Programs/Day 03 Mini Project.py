caa54ed52298527fb7ac1a8b08fc135693f37d93
silkyjain <jain.silky1702@gmail.com>
2024-03-09 20:57:34 -0500

    edited

--- Python Programs/Day 03 Mini Project.py
+++ Python Programs/Day 03 Mini Project.py
@@ -1,5 +1,7 @@
 # 1. BMI Calculator:
-# Create a Body Mass Index (BMI) calculator program. Prompt the user to enter their weight (in kilograms) and height (in meters), then calculate and print their BMI. Additionally, provide a message indicating whether they are underweight, normal weight, overweight, or obese based on the calculated BMI.
+# Create a Body Mass Index (BMI) calculator program. Prompt the user to enter their weight (in
+# kilograms) and height (in meters), then calculate and print their BMI. Additionally, provide a message indicating
+# whether they are underweight, normal weight, overweight, or obese based on the calculated BMI.
 
 # Provide a message indicating the BMI category based on the following ranges:
 # Underweight: BMI < 18.5
@@ -50,7 +52,8 @@ You are categorized as: Obesity'''
 
 
 # 2. Simple Calculator
-# Write a program to build a simple calculator that can perform addition, subtraction, multiplication and division operations.
+# Write a program to build a simple calculator that can perform addition, subtraction,
+# multiplication and division operations.
 
 # Solution
 print("Welcome to the Calculator")
